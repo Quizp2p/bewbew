ec489daf83c4f5f72a24bbca7a99e29c4bdf915c
Quiz42 <quizzhu@gmail.com>
2016-05-16 12:07:47 +0800

    First Sail!

---
+++ README.md
@@ -0,0 +1,165 @@
+# Bewbew Attack Map
+
+HanSight Animated Bewbew Attack Map v1.0
+
+## Quick Start
+
+### 使用用Bower 安装
+* 运行 `bower install bewbew --save`
+* 在页面的html文件中引用：
+
+````html
+<script src="bower_components/d3/d3.min.js"></script>
+<script src="bower_components/topojson/topojson.min.js"></script>
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/datamaps/dist/datamaps.world.min.js"></script>
+<script src="bower_components/moment/min/moment.min.js"></script>
+<script src="bower_components/perfect-scrollbar/js/perfect-scrollbar.jquery.min.js"></script>
+<script src="bower_components/elasticsearch/elasticsearch.jquery.js"></script>
+<script src="bower_components/bewbew/js/d3.geo.projection.v0.min.js"></script>
+<script src="bower_components/bewbew/js/bewbew.min.js"> </script>
+````
+* 加载默认的css样式:
+
+`````html
+<link rel="stylesheet" href="bower_components/perfect-scrollbar/css/perfect-scrollbar.min.css">
+<link rel="stylesheet" href="bower_components/bewbew/css/bewbew.css">
+`````
+* 通过修改bewbew.css文件可以定制地图的外观和布局:
+
+```css
+#attack_container {
+    background-color: black;
+    overflow-y:hidden;
+}
+
+#attackdiv {
+    font-family: monospace;
+    font-size: 10px;
+    position: fixed;
+    height: 100px;
+    width: 30%;
+    color: white;
+    background-color: black;
+    padding: : 5px;
+    margin-right: 30%;
+    bottom: 0px;
+    right: 0px;
+    text-align: left;
+    overflow-y: auto;
+}
+
+#container1 {
+    position: relative;
+    width: auto;
+    height: 100%;
+
+}
+
+```
+
+* 在页面中加入html元素:
+
+````html
+<div class="application" id="attack_container">
+
+    <center>
+        <div id="container1"></div>
+    </center>
+    <div id="attackdiv" class="perfect"></div>
+</div>
+````
+
+* 在global scope中调用函数：
+
+```javascript
+var bewbewConfig = {
+        demoMode: true,
+        esWindowSize: 5
+    };
+    bewbew(bewbewConfig);
+```
+
+其中 `bewbewConfigs` 是地图的配置项：
+
+````javascript
+bewbewConfig.demoMode = true; //开启Demo模式，地图将循环固定时间段里的数据播放动画
+bewbewConfig.lastTimestamp = '2016-01-01T00:00:00+08:00'; //Demo模式下固定时间段的起始点
+bewbewConfig.bufferSize = 30; //渲染攻击的buffer 大小
+bewbewConfig.esWindowSize = 100; //每次查询ES的Size
+bewbewConfig.animateRefreshTime = 1; //动画的刷新间隔时间
+bewbewConfig.esRefreshTime = 5; //查询ES的间隔时间
+bewbewConfig.indexPattern = 'saas_*';
+bewbewConfig.attackField = 'analysis'; //存储攻击的Field
+
+// 攻击信息里的颜色
+bewbewConfig.attackDivColors = {
+    text_color: '#4ec2d1',
+    alert_color: '#f0f6f6',
+    ip_color: '#a4429e'
+}
+
+//定义不同攻击类型的表现颜色
+bewbewConfig.attackColors = {
+    'cybercrime': '#f26060',
+    'blocklist_de_ssh': '#33e0f1',
+    'FileIncludeAttack ': '#f98206',
+    'stopforumspam_90d': '#bee248',
+    'FileAccess ': '#16f228'
+};
+
+//攻击地图里的颜色
+bewbewConfig.mapColors = {
+    fill_color: 'black',
+    border_color: '#4393c3',
+    highlight_fill_color: 'gray',
+    highlight_border_color: 'rgba(250, 15, 160, 0.2)',
+    hoverinfo_text: '#72cd3c',
+    hoverinfo_bg: '#393649'
+};
+
+//数据中心的经纬度坐标
+bewbewConfig.dcLocation = {
+    lat: '39.03',
+    lon: '117.68'
+};
+````
+
+## Elastic Search 中的数据
+
+可以参考`response.json`中elasticsearch返回的查询结果：
+
+````json
+{
+    "sc_win32_substatus": "0",
+    "@@uid": "netbinhai27anquanyia04",
+    "s_ip": {
+        "host": "192.168.1.143"
+    },
+    "s_port": "81",
+    "@timestamp": "2016-04-26T02:16:33.000Z",
+    "cs_User_Agent": "Mozilla/5.0+(Linux;+U;+Android+4.4.2;+zh-cn;+GT-I9500+Build/KOT49H)+AppleWebKit/537.36+(KHTML,+like+Gecko)Version/4.0+MQQBrowser/5.0+QQ-URL-Manager+Mobile+Safari/537.36",
+    "sc_status": "200",
+    "cs_method": "GET",
+    "analysis": "cybercrime",
+    "sc_substatus": "0",
+    "cs_uri_stem": "/lib/js/common.js",
+    "time_taken": "21561",
+    "c_ip": {
+        "regionl0": "\u4e2d\u56fd",
+        "regionl1": "\u4e0a\u6d77",
+        "regionl2": "\u4e0a\u6d77",
+        "ip": "101.226.33.228",
+        "host": "101.226.33.228",
+        "location": {
+            "lat": "31.224349",
+            "lon": "121.4767528"
+        }
+    }
+}
+````
+
+在给出的示例数据中攻击地图会找出所有涵 `analysis` field条目，然后会取该field中的值作为攻击类型;
+根据`@timestamp`生成攻击的时间序列；
+攻击来源地国家，地区，ip分别会读取 `c_ip.regionl0` ， `c_ip.regionl1`, `c_ip.ip` ;
+攻击在地图上的起始地址会根据 `c_ip.location` 中的经纬度坐标来确定.
---
+++ js/bewbew.js
@@ -0,0 +1,417 @@
+(function () {
+
+    var demoMode = false;
+    var lastTimestamp = '2016-01-01T00:00:00+08:00';
+
+    var bufferSize = 30;
+    var esWindowSize = 100;
+    var animateRefreshTime = 1;
+    var esRefreshTime = 5;
+    var indexPattern = 'saas_*';
+    var attackField = 'analysis';
+
+    var attackDivColors = {
+        text_color: '#4ec2d1',
+        alert_color: '#f0f6f6',
+        ip_color: '#a4429e'
+    }
+
+    var attackColors = {
+        'cybercrime': '#f26060',
+        'blocklist_de_ssh': '#33e0f1',
+        'FileIncludeAttack ': '#f98206',
+        'stopforumspam_90d': '#bee248',
+        'FileAccess ': '#16f228'
+    };
+
+    var mapColors = {
+        fill_color: 'black',
+        border_color: '#4393c3',
+        highlight_fill_color: 'gray',
+        highlight_border_color: 'rgba(250, 15, 160, 0.2)',
+        hoverinfo_text: '#72cd3c',
+        hoverinfo_bg: '#131a0e'
+    };
+
+    var dcLocation = {
+        lat: '39.03',
+        lon: '117.68'
+    };
+
+    var lastFrameTime = 0;
+    var lastMapupdateTime = 0;
+    var lastEsFetchTime = 0;
+
+    // Get elastic search
+    var elasticsearchHost = 'http://' + window.location.host + '/elasticsearch/';
+
+//  Declare an ES client in the IIFE funcion scope
+    var client;
+
+    var asyncLock = false;
+
+    function fetchES(nextStep) {
+        asyncLock = true;
+        client.search({
+            index: indexPattern,
+            size: esWindowSize,
+            //    type: '*',
+            body: {
+                "query": {
+                    "filtered": {
+                        "filter": {
+                            "and": {
+                                "filters": [{
+                                    "exists": {
+                                        "field": attackField
+                                    }
+                                }, {
+                                    "range": {
+                                        "@timestamp": {
+                                            "gt": lastTimestamp,
+                                            "lte": "now"
+                                        }
+                                    }
+                                }],
+                                "_cache": false
+                            }
+                        }
+                    }
+                },
+                "sort": {
+                    "@timestamp": "desc"
+                }
+            }
+        }).then(function(resp) {
+            var hits = resp.hits.hits;
+            // console.log("DEBUG: in thenning stage");
+            nextStep(hits);
+            asyncLock = false;
+        }, function(err) {
+            console.trace(err.message);
+            asyncLock = false;
+        });
+    }
+
+    // we maintain a fixed queue of "animations" via this class
+    function FixedQueue(size, initialValues) {
+        initialValues = (initialValues || []);
+        var queue = Array.apply(null, initialValues);
+        queue.fixedSize = size;
+        queue.push = FixedQueue.push;
+        queue.splice = FixedQueue.splice;
+        queue.unshift = FixedQueue.unshift;
+        FixedQueue.trimTail.call(queue);
+        return (queue);
+    }
+
+    FixedQueue.trimHead = function() {
+        if (this.length <= this.fixedSize) {
+            return;
+        }
+        Array.prototype.splice.call(this, 0, (this.length - this.fixedSize));
+    };
+
+    FixedQueue.trimTail = function() {
+        if (this.length <= this.fixedSize) {
+            return;
+        }
+        Array.prototype.splice.call(this, this.fixedSize, (this.length - this.fixedSize));
+    };
+
+    FixedQueue.wrapMethod = function(methodName, trimMethod) {
+        var wrapper = function() {
+            var method = Array.prototype[methodName];
+            var result = method.apply(this, arguments);
+            trimMethod.call(this);
+            return (result);
+        };
+        return (wrapper);
+    };
+
+    FixedQueue.push = FixedQueue.wrapMethod("push", FixedQueue.trimHead);
+    FixedQueue.splice = FixedQueue.wrapMethod("splice", FixedQueue.trimTail);
+    FixedQueue.unshift = FixedQueue.wrapMethod("unshift", FixedQueue.trimTail);
+
+
+    var hitsBuffer,
+        boomBuffer;
+
+
+
+
+    // the fun begins! =============================================================
+    //
+    // pretty simple setup ->
+    // * make base Datamap
+    // * setup timers to add random events to a queue
+    // * update the Datamap
+
+
+    var map = {};
+
+
+    function getStroke(attck) {
+        if (attck) {
+            return attackColors[attck];
+        }
+        if ((Math.round(Math.random() * 100) < 70)) {
+            return 'blue';
+        } else {
+            return 'red';
+        }
+    }
+
+    var results = [];
+
+    function main() {
+
+
+        var now = Date.now(),
+            EsDelay = (now - lastEsFetchTime) / 1000.0,
+            MapDelay = (now - lastMapupdateTime) / 1000.0;
+
+        //            console.log("DEBUG: Enter new frame!!!! Delta = " + dt);
+
+        //TODO: update Data and Map
+        if ((!asyncLock) && (EsDelay > esRefreshTime) && (results.length == 0)) {
+            //                console.log("DEBUG: call getData()");
+            //                console.log(results);
+            attacks.getData();
+        }
+
+        if ((results.length > 0) && (MapDelay > animateRefreshTime)) {
+            //                console.log("DEBUG: call updateMap()");
+            //                console.log(results);
+            //                console.log(hits);
+            //                console.log(boom);
+
+            attacks.updateMap();
+        }
+
+        lastFrameTime = now;
+        requestAnimationFrame(main);
+
+    }
+
+    function init() {
+
+        if (!demoMode) {
+            //
+            // console.log("DEBUG: current date time : " + moment().format());
+            lastTimestamp = moment().format();
+        }
+        client = new $.es.Client({
+            hosts: elasticsearchHost,
+            apiVersion: '1.7'
+                //    log: 'trace'
+        });
+
+        map = new Datamap({
+
+            scope: 'world',
+            element: document.getElementById('container1'),
+            projection: 'winkel3',
+            // change the projection to something else only if you have absolutely no cartographic sense
+
+            fills: {
+                defaultFill: mapColors.fill_color,
+            },
+
+            geographyConfig: {
+                dataUrl: null,
+                hideAntarctica: true,
+                borderWidth: 0.75,
+                borderColor: mapColors.border_color,
+                popupTemplate: function(geography, data) {
+                    return '<div class="hoverinfo" style="color:' + mapColors.hoverinfo_text + ';background:' + mapColors.hoverinfo_bg + '">' +
+                        geography.properties.name + '</div>';
+                },
+                popupOnHover: true,
+                highlightOnHover: false,
+                highlightFillColor: mapColors.highlight_fill_color,
+                highlightBorderColor: mapColors.highlight_border_color,
+                highlightBorderWidth: 2
+            },
+
+        });
+        hitsBuffer = FixedQueue(bufferSize, []);
+        boomBuffer = FixedQueue(bufferSize, []);
+
+        lastFrameTime = Date.now();
+        // console.log('DEBUG: set lastFrameTime in init(): ' + Date.now());
+        // console.log('DEBUG: call main() in attacks.init() -- start --->>');
+        //        setTimeout(jQuery.proxy(this.updateMap(), this), 5000);
+        attacks.getData();
+        main();
+
+        // console.log('DEBUG: call main() in attacks.init() -- end ----<<');
+
+    }
+
+    // doing this a bit fancy for a hack, but it makes it
+    // easier to group code functions together and have variables
+    // out of global scope
+    var attacks = {
+
+
+        updateMap: function() {
+
+            var strokeColor;
+
+            var result = results.shift();
+
+            strokeColor = getStroke(result.intel.which_attack);
+
+            hitsBuffer.push(result.hit);
+
+            //                console.log('DEBUG: draw pushed  hit------>');
+            //                console.log(result.hit);
+
+            // Draw the arcs
+            map.arc(hitsBuffer, {
+                strokeWidth: 2,
+                strokeColor: strokeColor
+            });
+
+            boomBuffer.push(result.boom);
+            //
+            //                console.log('DEBUG: draw pushed  boom------>');
+            //                console.log(result.boom);
+
+            // Draw the bubbles
+            map.bubbles(boomBuffer, {
+                popupTemplate: function(geo, data) {
+                    return '<div class="hoverinfo">' + data.attk + '</div>';
+                }
+            });
+
+
+            // update the scrolling attack div
+            var intel = result.intel;
+
+            if ($('#attackdiv').find(".attack_intel").length > bufferSize) {
+                $('#attackdiv').find(".attack_intel").slice(0, 1).remove();
+            }
+
+            $('#attackdiv').append("<div class='attack_intel' style=color:" + attackDivColors.text_color + " >" + intel.country + " ，" + intel.region + "<span class='attack_ip' style='color:" + attackDivColors.ip_color + " '> (" + intel.ip + ") </span> - " +
+                " <span  class='attack_alert' style='color:" + attackDivColors.alert_color + "'> 攻击类型: </span> " +
+                " <span class='attack_type' style='color:" + attackColors[intel.which_attack] + "'>  " + " " + intel.which_attack + " </span> " +
+                "</div>");
+            $('#attackdiv').animate({
+                scrollTop: $('#attackdiv').prop("scrollHeight")
+            }, 180);
+
+            lastMapupdateTime = Date.now();
+
+        },
+
+
+        getData: function() {
+            //            console.log(
+            //                'DEBUG: in getData Func'
+            //            );
+
+            // console.log("DEBUG:  in getData() function start to call fetchES")
+
+            fetchES(function(esResults) {
+                //            console.log(
+                //                    typeof results
+                //            )
+                if (lastTimestamp === esResults[0]._source['@timestamp']) {
+                    return;
+                }
+
+                if (!demoMode) {
+                    lastTimestamp = esResults[0]._source['@timestamp'];
+                }
+
+
+                // console.log(
+                //     'DEBUG: Last Time Stamp in @fetchES(L750): ' + lastTimestamp
+                // );
+
+                var result = {};
+
+                esResults.forEach(
+                    function(esResult) {
+                        var result = {};
+                        // add hit to the arc queue
+
+                        result.hit = {
+                            origin: {
+                                latitude: +esResult._source.c_ip.location.lat,
+                                longitude: +esResult._source.c_ip.location.lon
+                            },
+                            destination: {
+                                latitude: +dcLocation.lat,
+                                longitude: +dcLocation.lon
+                            }
+                        };
+
+
+                        // add boom to the bubbles queue
+
+                        result.boom = {
+                            radius: 7,
+                            latitude: +esResult._source.c_ip.location.lat,
+                            longitude: +esResult._source.c_ip.location.lon,
+                            fillOpacity: 0.5,
+                            attk: esResult._source.analysis
+                        };
+
+                        result.intel = {
+                            country: esResult._source.c_ip.regionl0,
+                            region: esResult._source.c_ip.regionl1,
+                            ip: esResult._source.c_ip.ip,
+                            which_attack: esResult._source.analysis
+                        };
+
+                        results.push(result);
+
+
+                    }
+                );
+
+            });
+
+            lastFrameTime = Date.now();
+
+        }
+
+    };
+
+
+
+
+
+    window.bewbew = function(config) {
+        if (config) {
+        demoMode = config.demoMode ? config.demoMode : demoMode;
+        lastTimestamp = config.lastTimestamp ?  config.lastTimestamp : lastTimestamp;
+        bufferSize = config.bufferSize ? config.bufferSize :  bufferSize;
+        esWindowSize = config.esWindowSize ? config.esWindowSize : esWindowSize;
+        animateRefreshTime = config.animateRefreshTime ? config.animateRefreshTime : animateRefreshTime;
+        esRefreshTime = config.esRefreshTime ? config.esRefreshTime : esRefreshTime;
+        indexPattern = config.indexPattern ? config.indexPattern : indexPattern;
+        attackField = config.attackField ? config.attackField: attackField;
+        attackDivColors = config.attackDivColors ? config.attackDivColors : attackDivColors;
+        attackColors = config.attackColors ? config.attackColors : attackColors;
+        mapColors = config.mapColors ? config.mapColors : mapColors;
+        dcLocation = config.dcLocation ? config.dcLocation : dcLocation;
+    }
+
+        // start the ball rolling!
+        $('.perfect').perfectScrollbar();
+        $(document).ready(function() {
+            init();
+        });
+
+        // lazy-dude's responsive window
+        d3.select(window).on('resize', function() {
+                location.reload();
+            });
+
+        };
+
+    }());
